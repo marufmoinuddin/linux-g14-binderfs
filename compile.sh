a435c8f3012622aafe528d4f6f07687eca1830f5
Moin Uddin Amed <56315433+marufmoinuddin@users.noreply.github.com>
2023-08-16 01:46:01 +0600

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create a temporary directory to hold package output
+mkdir -p package_output
+
+# Install required dependencies
+sudo pacman -Sy --noconfirm base-devel devtools wget git --overwrite '*'
+
+# Clone the repository
+git clone https://aur.archlinux.org/linux-g14.git
+
+# Install extra dependencies needed for building the kernel
+makedepends=$(awk '/^makedepends=\(/,/\)/ {gsub(/[\(\)]/,""); gsub(/^makedepends= /, ""); print}' linux-g14/PKGBUILD | tr -d '\n' | sed 's/#.*//; s/  */ /g' | sed 's/makedepends=//')
+sudo pacman -Sy --noconfirm $makedepends --needed --overwrite '*'
+
+# Build the package using makepkg
+cp add-lines.sh linux-g14/add-lines.sh
+chmod +x linux-g14/add-lines.sh
+sh linux-g14/add-lines.sh
+cd linux-g14
+makepkg -s --noconfirm --skippgpcheck
+
+# Move the generated package files 
+mv *.pkg.tar.zst ../package_output/
+
